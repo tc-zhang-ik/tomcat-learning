2a810b787f5eef4b58b9b00c8fd2b115a4938e29
tczhang <1518622711@qq.com>
2025-02-15 16:01:49 +0800

    添加 pom.xml

--- java/org/apache/catalina/core/StandardServer.java
+++ java/org/apache/catalina/core/StandardServer.java
@@ -465,6 +465,7 @@ public final class StandardServer extends LifecycleMBeanBase implements Server {
                     InputStream stream;
                     long acceptStartTime = System.currentTimeMillis();
                     try {
+                        //阻塞等待
                         socket = serverSocket.accept();
                         socket.setSoTimeout(10 * 1000); // Ten seconds
                         stream = socket.getInputStream();
